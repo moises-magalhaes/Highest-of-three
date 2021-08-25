74877e1836f44cbe58fe809addea735369afb202
moises-magalhaes <63241484+moises-magalhaes@users.noreply.github.com>
2021-08-25 16:05:09 -0300

    not matching total

--- scripts/model1.js
+++ scripts/model1.js
@@ -5,30 +5,70 @@ import { numsNegativeArray } from "./array.js";
 numsNegativeArray.sort();
 // console.log(numsNegativeArray);
 
-const maxProduct = (numsNegativeArray = []) => {
-	const sorter = (a, b) => b - a;
-	numsNegativeArray.sort(sorter);
-	console.log(numsNegativeArray);
+// let maxProd = 0;
+//------------------------------------------------------------//
+// const sorter = (a, b) => b - a;
+// numsNegativeArray.sort(sorter);
+// console.log(numsNegativeArray);
+//---------------------------------------------------------------//
+let arr = numsNegativeArray;
+console.log(arr);
 
-	let product1 = 1,
-		product2 = 1;
-	let len = numsNegativeArray.length - 1;
+const HighestOf3 = (arr) => {
+	let highest = Math.max(arr[0], arr[1]);
+	console.log(highest);
+	let lowest = Math.min(arr[0], arr[1]);
+	console.log(lowest);
+	let highestProductOf2 = arr[0] * arr[1];
+	console.log(highestProductOf2);
+	let lowestProductOf2 = arr[0] * arr[1];
+	console.log(lowestProductOf2);
+	let highestProductOf3 = arr[0] * arr[1] * arr[2];
+	console.log(highestProductOf3);
 
-	for (let i = len; i > len - 3; i--) {
-		console.log((product1 = product1 * numsNegativeArray[i]));
+	for (let i = 2; i < arr.length; i++) {
+		let current = arr[i];
+		console.log(current);
+		highestProductOf3 = Math.max(
+			highestProductOf3,
+			current * highestProductOf2,
+			current * lowestProductOf2
+		);
 
-		return (product1 = product1 * numsNegativeArray[i]);
-		// console.log(
-		// 	numsNegativeArray[0] * numsNegativeArray[1] * numsNegativeArray[2]
-		// );
+		console.log(highestProductOf3);
+
+		highestProductOf2 = Math.max(
+			highestProductOf2,
+			current * highest,
+			current * lowest
+		);
+
+		console.log(highestProductOf2);
+
+		lowestProductOf2 = Math.min(
+			lowestProductOf2,
+			current * highest,
+			current * lowest
+		);
+
+		console.log(lowestProductOf2);
+
+		highest = Math.max(highest, current);
+		console.log(highest);
+
+		lowest = Math.min(lowest, current);
+
+		console.log(lowest);
 	}
 
-	product2 =
-		numsNegativeArray[0] * numsNegativeArray[1] * numsNegativeArray[len];
-
-	// console.log(product2);
-
-	return Math.max(product1, product2);
+	return highestProductOf3;
 };
+console.log(HighestOf3(arr));
 
-console.log(maxProduct(numsNegativeArray));
+// const maxProduct = (numsNegativeArray = []) => {
+// 	const sorter = (a, b) => b - a;
+// 	numsNegativeArray.sort(sorter);
+// 	console.log(numsNegativeArray);
+// };
+
+// console.log(maxProduct(numsNegativeArray));
